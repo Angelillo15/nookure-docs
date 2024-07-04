b1ca70359c0e6ab311224b7cf8fbca56f504ef44
Angelillo15 <contact@angelillo15.es>
2024-07-04 18:44:26 +0200

    Add NookureInventory section

---
+++ .vitepress/nookureInventory.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from 'vitepress';
+
+const inventory = [
+  {
+    text: 'Getting Started',
+    collapsed: false,
+    items: [
+      { text: 'What is Nookure Inventory?', link: 'what-is-nookureinventory' },
+      { text: 'Setting up NookureInventory', link: 'setup' },
+      { text: 'Creating our first gui', link: 'first-gui' },
+      { text: 'What is a template engine?', link: 'what-is-a-template-engine' },
+
+    ]
+  },
+] as DefaultTheme.SidebarItem[];
+
+export default inventory;
